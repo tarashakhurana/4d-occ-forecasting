e8f262721a647e6e65dbf0cf9d12804f7087d579
Tarasha Khurana <tkhurana@andrew.cmu.edu>
2023-04-05 15:33:09 -0400

    add cuda support for chamferdist

--- utils/chamferdist/setup.py
+++ utils/chamferdist/setup.py
@@ -1,20 +1,47 @@
 import os
 import runpy
+
 import torch
 from setuptools import find_packages, setup
-from torch.utils.cpp_extension import CppExtension
+from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
 
 
 def get_extensions():
     include_dirs = ["chamferdist"]
     main_source = os.path.join("chamferdist", "ext.cpp")
     sources = [os.path.join("chamferdist", "knn_cpu.cpp")]
+    source_cuda = [os.path.join("chamferdist", "knn.cu")]
     sources = [main_source] + sources
 
     extension = CppExtension
 
     extra_compile_args = {"cxx": ["-std=c++14"]}
     define_macros = []
+
+    force_cuda = os.getenv("FORCE_CUDA", "0") == "1"
+    if (torch.cuda.is_available() and CUDA_HOME is not None) or force_cuda:
+        extension = CUDAExtension
+        sources += source_cuda
+        define_macros += [("WITH_CUDA", None)]
+        nvcc_args = [
+            "-DCUDA_HAS_FP16=1",
+            "-D__CUDA_NO_HALF_OPERATORS__",
+            "-D__CUDA_NO_HALF_CONVERSIONS__",
+            "-D__CUDA_NO_HALF2_OPERATORS__",
+        ]
+        nvcc_flags_env = os.getenv("NVCC_FLAGS", "")
+        if nvcc_flags_env != "":
+            nvcc_args.extend(nvcc_flags_env.split(" "))
+
+        CC = os.environ.get("CC", None)
+        if CC is not None:
+            CC_arg = "-ccbin={}".format(CC)
+            if CC_arg not in nvcc_args:
+                if any(arg.startswith("-ccbin") for arg in nvcc_args):
+                    raise ValueError("Inconsistent ccbins")
+                nvcc_args.append(CC_arg)
+
+        extra_compile_args["nvcc"] = nvcc_args
 
     ext_modules = [
         extension(
@@ -51,7 +78,7 @@ setup(
     version=__version__,
     description="Pytorch Chamfer distance",
     packages=find_packages(),
-    package_data={'chamferdist': ['*.h']},
+    package_data={'chamferdist': ['*.cu', '*.cuh', '*.h']},
     long_description=long_description,
     install_requires=[],
     extras_require={
